47da0ed359222ec8953b9f518c4875b89ca5aa3d
barbbella <barbedbella@gmail.com>
2015-05-04 21:37:04 -0700

    Initial commit

--- pathfinder.js
+++ pathfinder.js
@@ -2,15 +2,47 @@
 
 function getNodeWithLeastTraversalCost(nodes){
   // return node with least traversalCost
-};
+  // traverse array of nodes
+  // check node.traversalCost
+  // return node of lowest
+  var low = nodes[0] || null;
+  for (var i = 1; i<nodes.length; i++) {
+    if (nodes[i].traversalCost < low.traversalCost) {
+      low = nodes[i];
+    }
+  }
+  return low;
+}
 
-function findShortestPath(currentNode, goalNode){ // where nodes between start and end have tentative costs.
+function findShortestPath(currentNode, goalNode, path){ // where nodes between start and end have tentative costs.
   // return shortest path array based on graph of nodes with traversalCosts assigned
-};
 
-function dijkstra(startNode, endNode){
+  path = path || [];
+
+  if (currentNode === goalNode) {
+    path.unshift(currentNode);
+    return path;
+  }
+
+  if (currentNode.neighbors.length < 1) {
+
+    return null;
+  }
+
+  // recursive case
+  // find cheapest neighbor
+  path.unshift(currentNode);
+  currentNode = getNodeWithLeastTraversalCost(currentNode.neighbors);
+  return findShortestPath(currentNode, goalNode, path);
+
+}
+
+function dijkstra(startNode, endNode, path){
   // return shortest path array of nodes
-};
+
+
+
+}
 
 // Nodes for Graphs/Maps
 function Node(name, cost){
